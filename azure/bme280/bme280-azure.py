b51e0038dcbe49664c8d69c004c0aa9decbd808f
Scott Baker <smbaker@gmail.com>
2021-02-18 16:07:27 -0800

    cloud-to-device control of interval

--- azure/bme280/bme280-azure.py
+++ azure/bme280/bme280-azure.py
@@ -1,17 +1,46 @@
+import threading
 import time
 
 from bme280 import BME280
 from smbus import SMBus
-from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
 CONNECTION_STRING=open("/etc/azure/device-connection").readline().strip()
 
 MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity},"pressure": {pressure}}}'
 
+INTERVAL = 30
+
 def iothub_client_init():
     # Create an IoT Hub client
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
     return client
+
+def device_method_listener(device_client):
+    global INTERVAL
+    while True:
+        method_request = device_client.receive_method_request()
+        print (
+            "\nMethod callback called with:\nmethodName = {method_name}\npayload = {payload}".format(
+                method_name=method_request.name,
+                payload=method_request.payload
+            )
+        )
+        if method_request.name == "SetTelemetryInterval":
+            try:
+                INTERVAL = int(method_request.payload)
+            except ValueError:
+                response_payload = {"Response": "Invalid parameter"}
+                response_status = 400
+            else:
+                response_payload = {"Response": "Executed direct method {}".format(method_request.name)}
+                response_status = 200
+        else:
+            response_payload = {"Response": "Direct method {} not defined".format(method_request.name)}
+            response_status = 404
+
+        method_response = MethodResponse(method_request.request_id, response_status, payload=response_payload)
+        device_client.send_method_response(method_response)
 
 def main():
     bus = SMBus(1)
@@ -19,6 +48,11 @@ def main():
     try:
         client = iothub_client_init()
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        # Start a thread to listen 
+        device_method_thread = threading.Thread(target=device_method_listener, args=(client,))
+        device_method_thread.daemon = True
+        device_method_thread.start()
 
         while True:
             # Build the message with simulated telemetry values.
@@ -37,11 +71,13 @@ def main():
             else:
                 message.custom_properties["temperatureAlert"] = "false"
 
+            message.custom_properties["kind"] = "bme280"
+
             # Send the message.
             print( "Sending message: {}".format(message) )
             client.send_message(message)
             print ( "Message successfully sent" )
-            time.sleep(30)
+            time.sleep(INTERVAL)
 
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
