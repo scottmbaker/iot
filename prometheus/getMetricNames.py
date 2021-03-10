6b44894f20086d8c301aa51f4b42c2f8ce8caf64
Scott Baker <smbaker@gmail.com>
2021-03-09 16:43:20 -0800

    tool to get metric names

---
+++ prometheus/getMetricNames.py
@@ -0,0 +1,18 @@
+import csv
+import requests
+import sys
+
+URL="http://localhost:9090"
+
+def GetMetricNames(url):
+    response = requests.get('{0}/api/v1/label/__name__/values'.format(url))
+    names = response.json()['data']
+    #Return metrix names
+    return names
+
+def main():
+    mn = GetMetricNames(URL)
+    print("\n".join(mn))
+
+if __name__=="__main__":
+    main()
